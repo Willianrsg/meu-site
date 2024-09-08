ba5b7ac86c44693d70d812cd82ac9d126a7f9c77
Willian Rafael <willian.rafael18@gmail.com>
2024-09-08 20:50:41 -0300

    fix: Corrigido o menu mobile

--- main.js
+++ main.js
@@ -23,3 +23,25 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 });
 
+function updateFooterYear() {
+  const year = new Date().getFullYear(); 
+  const footerText = document.getElementById('footer-text');
+  footerText.innerHTML = `Desenvolvido por: Willian Garcia | WDN Company 2010 - ${year}`;
+}
+
+document.addEventListener('DOMContentLoaded', updateFooterYear);
+
+document.addEventListener('DOMContentLoaded', function() {
+  const menuToggle = document.getElementById('menu-toggle');
+  const mobileMenu = document.getElementById('mobile-menu');
+
+  menuToggle.addEventListener('click', function() {
+    if (mobileMenu.classList.contains('hidden')) {
+      mobileMenu.classList.remove('hidden');
+    } else {
+      mobileMenu.classList.add('hidden');
+    }
+  });
+});
+
+
