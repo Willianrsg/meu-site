5b82f6e7016ce4a456db9a21aeea1aec58842e72
Willian Rafael <willian.rafael18@gmail.com>
2024-09-07 23:50:48 -0300

    -Criando Projeto

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  fontFamily: {
+    'sans': ['Bebas', 'sans-serif', '']
+  },
+  theme: {
+    extend: {
+      backgroundImage: {
+        "home": "url('/assets/bg.png')",
+      }
+    },
+  },
+  plugins: [],
+}
