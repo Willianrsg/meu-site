3a882e8e81d44cc4f50752ce1766eeb6c922f63d
Willian Rafael <willian.rafael18@gmail.com>
2024-09-08 17:51:34 -0300

    ADICIONADO PAGINAÇÃO

---
+++ main.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 30
+        }
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+      }
+    });
+  });
+
